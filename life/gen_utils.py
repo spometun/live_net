64e02a9bdc614850d61c591732b22b5c55f00b82
Sergiy Pometun <spometun@solinkcorp.com>
2023-03-08 11:42:31 -0500

    wip

--- life/gen_utils.py
+++ life/gen_utils.py
@@ -8,27 +8,20 @@ class IndexTakerGenerator:
 
     def __iter__(self):
         class _IndexTaker:
-            def __init__(self, data, index):
-                self._data = data
-                self._index = index
-                self._cur = -1
+            def __init__(self, parent):
+                self._iter = iter(parent._data)
+                self._parent = parent
 
             def __next__(self):
-                self._cur += 1
-                if self._cur >= len(self._data):
-                    raise StopIteration
-                return self[self._cur]
-
-            def __getitem__(self, item):
-                return self._data[item][self._index]
+                return next(self._iter)[self._parent._index]
 
             def __len__(self):
-                return len(self._data)
+                return self._parent.__len__()
 
             def __iter__(self):
                 return self
 
-        return _IndexTaker(self._data, self._index)
+        return _IndexTaker(self)
 
     def __len__(self):
         return len(self._data)
@@ -41,3 +34,23 @@ def split_dataset(dataset) -> tuple:
     data = IndexTakerGenerator(dataset, 0)
     labels = IndexTakerGenerator(dataset, 1)
     return data, labels
+
+
+'''
+class _IndexTaker:
+    def __init__(self, parent):
+        self._cur = -1
+        self._parent = parent
+
+    def __next__(self):
+        self._cur += 1
+        if self._cur >= self.__len__():
+            raise StopIteration
+        return self._parent[self._cur]
+
+    def __len__(self):
+        return self._parent.__len__()
+
+    def __iter__(self):
+        return self
+'''
