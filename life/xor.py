6fe51fb0cd5471313412533b357e68b742eeaaee
Sergiy Pometun <sergiy.pometun@gmail.com>
2023-03-17 10:54:46 -0400

    wip

--- life/xor.py
+++ life/xor.py
@@ -14,8 +14,17 @@ def test_dev():
     print("O merciful God")
 
 
-class Neuron:
+class Visitable:
+    def visit(self, func_name: str):
+        try:
+            exec(f"self.{func_name}()")
+        except AttributeError:
+            pass
+
+
+class Neuron():
     def __init__(self, module: "LiveNet"):
+        super().__init__()
         self.module = module
         self._output = None
 
@@ -25,6 +34,7 @@ class Neuron:
         return self._output
 
     def clear_output(self):
+        print(f"clear {type(self)} {self._output is None}")
         self._output = None
 
     @abc.abstractmethod
@@ -52,20 +62,27 @@ class DestinationNeuron(Neuron):
         synapse = Synapse(source, self)
         return synapse
 
+    def clear_output(self):
+        print(f"try {type(self)} {self._output is None}")
+        if self._output is not None:
+            for synapse in self.dendrites:
+                synapse.source.clear_output()
+            super().clear_output()
+
 
 class DataNeuron(SourceNeuron):
     def __init__(self, module: "LiveNet"):
         super().__init__(module)
 
     def set_output(self, value: torch.Tensor):
-        assert len(value.shape) == 2
+        assert len(value.shape) == 1
         self._output = value
 
     def _compute_output(self) -> torch.Tensor:
         raise NotImplementedError("Should never be called")
 
 
-class Synapse:
+class Synapse():
     def __init__(self, source: SourceNeuron, destination: DestinationNeuron):
         assert source != destination
         self.source = source
@@ -96,20 +113,33 @@ class RegularNeuron(SourceNeuron, DestinationNeuron):
 
 
 class LiveNet(nn.Module):
-    def __init__(self, n_inputs, n_outputs):
+    def __init__(self, n_inputs, n_middle, n_outputs):
         super().__init__()
         self._n_params = 0
-        self.inputs = [DataNeuron(self) for i in range(n_inputs)]
-        self.outputs = [RegularNeuron(self) for i in range(n_outputs)]
+        self.inputs = [DataNeuron(self) for _ in range(n_inputs)]
+        self.outputs = [RegularNeuron(self) for _ in range(n_outputs)]
+        '''
         for input_ in self.inputs:
             for output in self.outputs:
                 input_.connect_to(output)
+                '''
+        for i in range(n_middle):
+            neuron = RegularNeuron(self)
+            for input_ in self.inputs:
+                input_.connect_to(neuron)
+            for output in self.outputs:
+                output.connect_from(neuron)
         self.p = nn.Parameter(torch.tensor(0.0))
 
     def forward(self, x):
         assert len(x.shape) == 2, "Invalid input shape"
         assert x.shape[1] == len(self.inputs), "Invalid input dimension"
-        pass
+        for output in self.outputs:
+            output.clear_output()
+        for i in range(x.shape[1]):
+            self.inputs[i].set_output(x[:, i])
+        for output in self.outputs:
+            output.compute_output()
 
     def obtain_float_parameter(self, name_prefix: str) -> nn.Parameter:
         name = f"{name_prefix}{self._n_params}"
@@ -121,7 +151,10 @@ class LiveNet(nn.Module):
 
 if __name__ == "__main__":
     lib.utils.set_seed()
-    net = LiveNet(2, 1)
+    x = torch.tensor([[0., 0], [0, 1], [1, 0], [1, 1]])
+    net = LiveNet(2, 1, 2)
+    net.forward(x)
+    net.forward(x)
     s = pickle.dumps(net.p)
     p = [p for p in net.named_parameters()]
 
