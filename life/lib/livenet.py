e04f0118f78c84d8fd863b95c1b6575df1c185ef
Sergiy Pometun <spometun@solinkcorp.com>
2023-03-24 12:34:53 -0400

    odd

--- life/lib/livenet.py
+++ life/lib/livenet.py
@@ -8,29 +8,22 @@ import torch
 import torch.nn as nn
 import random
 import pickle
-from lib.graph import GraphNode, NodesHolder
+from life.lib.graph import GraphNode, NodesHolder
+import life.lib.utils as utils
 
 
-import lib
-from lib.simple_log import LOG
+from life.lib.simple_log import LOG
+import life.lib.optimizer as optimizer
 
 
 def test_dev():
     print("O merciful God")
 
 
-class Visitable:
-    def visit(self, func_name: str):
-        try:
-            exec(f"self.{func_name}()")
-        except AttributeError:
-            pass
-
-
 class Neuron(GraphNode):
-    def __init__(self, module: "LiveNet"):
+    def __init__(self, context: "Context"):
         super().__init__()
-        self.module = module
+        self.context = context
         self._output = None
 
     @typing.final
@@ -48,8 +41,8 @@ class Neuron(GraphNode):
 
 # noinspection PyAbstractClass
 class SourceNeuron(Neuron):
-    def __init__(self, module: "LiveNet"):
-        super().__init__(module)
+    def __init__(self, context: "Context"):
+        super().__init__(context)
         self.axons: List[Synapse] = []
 
     def connect_to(self, destination: "DestinationNeuron"):
@@ -61,57 +54,12 @@ class SourceNeuron(Neuron):
         return []
 
 
-# noinspection PyAbstractClass
 class DestinationNeuron(Neuron):
-    def __init__(self, module: "LiveNet"):
-        super().__init__(module)
+    def __init__(self, context: "Context"):
+        super().__init__(context)
         self.dendrites: List[Synapse] = []
-
-    def connect_from(self, source: SourceNeuron):
-        synapse = Synapse(source, self)
-        return synapse
-
-    @override
-    def get_adjacent_nodes(self) -> List[GraphNode]:
-        return self.dendrites
-
-
-class DataNeuron(SourceNeuron):
-    def __init__(self, module: "LiveNet"):
-        super().__init__(module)
-
-    def set_output(self, value: torch.Tensor):
-        assert len(value.shape) == 1
-        self._output = value
-
-    def _compute_output(self) -> torch.Tensor:
-        raise RuntimeError("Should never be called")
-
-
-class Synapse(GraphNode):
-    def __init__(self, source: SourceNeuron, destination: DestinationNeuron):
-        assert source != destination
-        self.source = source
-        self.destination = destination
-        assert source not in (synapse.source for synapse in destination.dendrites), "Connection already exists"
-        destination.dendrites.append(self)
-        assert destination not in (synapse.destination for synapse in source.axons), "Connection already exists"
-        source.axons.append(self)
-        self.k = source.module.obtain_float_parameter("s")
-        self.k.data = torch.tensor(random.random())
-
-    def output(self):
-        output = self.k * self.source.compute_output()
-        return output
-
-    def get_adjacent_nodes(self) -> List[GraphNode]:
-        return [self.source]
-
-
-class RegularNeuron(DestinationNeuron, SourceNeuron):
-    def __init__(self, module: "LiveNet"):
-        super().__init__(module)
-        self.b = module.obtain_float_parameter("n")
+        self.b = context.obtain_float_parameter("n")
+        self.optimizer = optimizer.SGD1(self.b)
 
     def _compute_output(self) -> torch.Tensor:
         if len(self.dendrites) == 0:
@@ -124,14 +72,77 @@ class RegularNeuron(DestinationNeuron, SourceNeuron):
         output = torch.relu(output)
         return output
 
+    def connect_from(self, source: SourceNeuron):
+        synapse = Synapse(source, self)
+        return synapse
+
+    @override
+    def get_adjacent_nodes(self) -> List[GraphNode]:
+        return self.dendrites
+
+
+class DataNeuron(SourceNeuron):
+    def __init__(self, context: "Context"):
+        super().__init__(context)
+
+    def set_output(self, value: torch.Tensor):
+        assert len(value.shape) == 1
+        self._output = value
+
+    def _compute_output(self) -> torch.Tensor:
+        raise RuntimeError("Should never be called")
+
+
+class RegularNeuron(DestinationNeuron, SourceNeuron):
+    def __init__(self, context: "Context"):
+        super().__init__(context)
+
+
+class Synapse(GraphNode):
+    def __init__(self, source: SourceNeuron, destination: DestinationNeuron):
+        assert source != destination
+        self.source = source
+        self.destination = destination
+        assert source not in (synapse.source for synapse in destination.dendrites), "Connection already exists"
+        destination.dendrites.append(self)
+        assert destination not in (synapse.destination for synapse in source.axons), "Connection already exists"
+        source.axons.append(self)
+        self.k = source.context.obtain_float_parameter("s")
+        with torch.no_grad():
+            self.k[...] = torch.tensor(random.random())
+        self.optimizer = optimizer.SGD1(self.k)
+
+    def output(self):
+        output = self.k * self.source.compute_output()
+        return output
+
+    def get_adjacent_nodes(self) -> List[GraphNode]:
+        return [self.source]
+
+
+class Context:
+    def __init__(self, module: nn.Module):
+        self.module = module
+        self._n_params = 0
+        self._name_counters = {}
+
+    def obtain_float_parameter(self, name_prefix: str) -> nn.Parameter:
+        if name_prefix not in self._name_counters:
+            self._name_counters[name_prefix] = -1
+        self._name_counters[name_prefix] += 1
+        name = f"{name_prefix}{self._name_counters[name_prefix]}"
+        self._n_params += 1
+        param = nn.Parameter(torch.tensor(0.0))
+        self.module.register_parameter(name, param)
+        return param
+
 
 class LiveNet(nn.Module):
     def __init__(self, n_inputs, n_middle, n_outputs):
         super().__init__()
-        self._n_params = 0
-        self._name_counters = {}
-        self.inputs = [DataNeuron(self) for _ in range(n_inputs)]
-        self.outputs = [RegularNeuron(self) for _ in range(n_outputs)]
+        self.context = Context(self)
+        self.inputs = [DataNeuron(self.context) for _ in range(n_inputs)]
+        self.outputs = [DestinationNeuron(self.context) for _ in range(n_outputs)]
         self.root = NodesHolder(self.outputs)
         '''
         for input_ in self.inputs:
@@ -139,7 +150,7 @@ class LiveNet(nn.Module):
                 input_.connect_to(output)
                 '''
         for i in range(n_middle):
-            neuron = RegularNeuron(self)
+            neuron = RegularNeuron(self.context)
             for input_ in self.inputs:
                 input_.connect_to(neuron)
             for output in self.outputs:
@@ -164,23 +175,13 @@ class LiveNet(nn.Module):
             y[:, i] = output.compute_output()
         return y
 
-    def obtain_float_parameter(self, name_prefix: str) -> nn.Parameter:
-        if name_prefix not in self._name_counters:
-            self._name_counters[name_prefix] = -1
-        self._name_counters[name_prefix] += 1
-        name = f"{name_prefix}{self._name_counters[name_prefix]}"
-        self._n_params += 1
-        param = nn.Parameter(torch.tensor(0.0))
-        self.register_parameter(name, param)
-        return param
-
 
 def export_onnx(model: nn.Module, dummy_input):
     torch.onnx.export(model, dummy_input, "/home/spometun/model.onnx", verbose=False)
 
 
 if __name__ == "__main__":
-    lib.utils.set_seed()
+    utils.set_seed()
     x = torch.tensor([[0., 0], [0, 1], [1, 0], [1, 1]])
     net = LiveNet(2, 2, 1)
     net.forward(x)
