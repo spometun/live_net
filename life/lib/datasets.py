28235837adc64e29f66d233297262033f3bd2eb3
Sergiy Pometun <spometun@solinkcorp.com>
2023-03-25 11:45:05 -0400

    refactor

---
+++ life/lib/datasets.py
@@ -0,0 +1,17 @@
+import torch
+
+
+def get_xor():
+    xor_x = torch.Tensor([[0., 0.],
+                   [0., 1.],
+                   [1., 0.],
+                   [1., 1.]])
+
+    xor_y = torch.Tensor([0., 1., 1., 0.]).reshape(xor_x.shape[0], 1)
+    return xor_x, xor_y
+
+
+def get_odd():
+    odd_x = torch.Tensor([[0.], [1.], [2.]])
+    odd_y = torch.Tensor([[0.], [1.], [0.]])
+    return odd_x, odd_y
