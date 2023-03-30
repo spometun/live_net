f24be4abffcac1866fc030548eb809011a174ca7
Sergiy Pometun <sergiy.pometun@gmail.com>
2023-03-30 16:25:32 -0400

    challenge

--- life/lib/utils.py
+++ life/lib/utils.py
@@ -46,6 +46,24 @@ def get_parameters_copy(network: torch.nn.Module):
     return params
 
 
+def get_parameters_dict(network: torch.nn.Module, clone=True):
+    res = {}
+    for name, param in network.named_parameters():
+        param = param.detach()
+        if clone:
+            param = param.clone()
+        res[name] = param.numpy()
+    return res
+
+
+def get_gradients_dict(network: torch.nn.Module):
+    res = {}
+    for name, param in network.named_parameters():
+        with torch.no_grad():
+            res[name] = param.grad
+    return res
+
+
 def set_parameters(network: torch.nn.Module, params: list):
     net_params = [p for p in network.parameters()]
     assert len(net_params) == len(params)
