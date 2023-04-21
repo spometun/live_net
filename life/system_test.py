073b6d7a785311dd51637fa06cceb9b4767d8af0
Sergiy Pometun <spometun@solinkcorp.com>
2023-04-21 18:04:23 -0400

    + unittest

--- life/system_test.py
+++ life/system_test.py
@@ -15,24 +15,7 @@ import pytest
 
 
 def test_bug():
-    print(torch.__version__)
-    np.set_printoptions(precision=3)
-    lib.utils.set_seed()
-    network = lib.livenet.LiveNet(1, None, 2)
-    # network.zero_grad(False)
-    network.on_grad_update()
+    pass
 
-
-def test_die():
-    module = torch.nn.Module()
-    context = lib.livenet.Context(module, 42)
-    src = lib.livenet.RegularNeuron(context, activation=torch.nn.ReLU())
-    neuron = lib.livenet.RegularNeuron(context, activation=torch.nn.ReLU)
-    dst = lib.livenet.DestinationNeuron(context, activation=None)
-    src.connect_to(neuron)
-    neuron.connect_to(dst)
-    dst.dendrites[0].die()
-    assert context.death_stat.dangle_neurons == 1
-    assert len(src.axons) == 0
 
 
