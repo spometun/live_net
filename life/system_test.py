04cd508f80ccb2bba913d25483176f39b1e368d4
Sergiy Pometun <spometun@solinkcorp.com>
2023-04-21 14:17:54 -0400

    bug getting closer

--- life/system_test.py
+++ life/system_test.py
@@ -15,38 +15,23 @@ import pytest
 
 
 def test_bug():
-    plt.ion()
-    lib.utils.set_seed()
     print(torch.__version__)
     np.set_printoptions(precision=3)
     lib.utils.set_seed()
-    downscale = 28
-    train_x, train_y = lib.datasets.to_plain_odd(*lib.datasets.get_mnist_test(), downscale=downscale)
-    network = lib.livenet.LiveNet(784 // (downscale * downscale), 1, 2)
+    network = lib.livenet.LiveNet(1, 1, 2)
+    network.zero_grad(False)
 
-    batch_iterator = lib.gen_utils.batch_iterator(train_x, train_y, batch_size=100)
-    criterion = lib.nets.criterion_n
-    optimizer = lib.nets.create_optimizer(network)
-    optimizer.learning_rate = 0.001
-    trainer = lib.trainer.Trainer(network, batch_iterator, criterion, optimizer, epoch_size=100)
-
-    network.context.alpha_l1 = 0.9
-
-    # network.inputs[0].axons[0].die()
-    # network.outputs[1].dendrites[0].die()
-    # network.outputs[0].dendrites[0].die()
-    #
-    optimizer.zero_grad()
-    optimizer.step()
-    # network.on_grad_update()
-    # trainer.step(500)
-
-    # for src in network.inputs:
-    #     for s in src.axons:
-    #         s.die()
-    # for dest in network.outputs:
-    #     for s in dest.dendrites:
-    #         s.die()
+    if False:
+        network.outputs[0].optimizer.step()
+        network.outputs[0].dendrites[0].optimizer.step()
+        network.outputs[0].dendrites[0].die()
+        network.inputs[0].axons[0].optimizer.step()
+        network.inputs[0].axons[0].die()
+        network.outputs[1].optimizer.step()
+        network.outputs[1].dendrites[0].optimizer.step()
+        network.outputs[1].dendrites[0].die()
+    else:
+        network.on_grad_update()
 
 
 def test_die():
