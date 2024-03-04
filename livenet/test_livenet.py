062005f35c5f62f581e44bc566a897f5515557ea
Serhii Pometun <sergiy.pometun@gmail.com>
2024-03-04 07:06:06 -0500

    wipb

--- livenet/test_livenet.py
+++ livenet/test_livenet.py
@@ -1,9 +1,11 @@
+__package__ = "livenet"
 import torch
 
 from .core.livenet import Context, RegularNeuron, DestinationNeuron, SourceNeuron, DataNeuron
 from .core import optimizers, livenet
 from . import nets, gen_utils, net_trainer
 from . import datasets
+from simple_log import LOG
 
 
 def test_die():
@@ -12,7 +14,7 @@ def test_die():
     src = DataNeuron(context)
     src.set_output(torch.Tensor(42))
     neuron = RegularNeuron(context, activation=torch.nn.ReLU)
-    return
+    exit(0)
     dst = DestinationNeuron(context, activation=None)
     src.connect_to(neuron)
     neuron.connect_to(dst)
@@ -38,19 +40,5 @@ def test_system_die_all():
     assert len(network.outputs[1].dendrites) == 0
 
 
-# def test_system():
-#     # core.simple_log.level = core.simple_log.LogLevel.DEBUG
-#     train_x, train_y = datasets.get_odd_2()
-#     context = livenet.Context()
-#     context.liveness_die_after_n_sign_changes = 2
-#     context.regularization_l1 = 0.01
-#     network = nets.create_livenet_odd_2(context)
-#     batch_iterator = gen_utils.batch_iterator(train_x, train_y, batch_size=len(train_x))
-#     criterion = nets.criterion_classification_n
-#     optimizer = optimizers.LiveNetOptimizer(network, lr=0.05)
-#     trainer = net_trainer.NetTrainer(network, batch_iterator, criterion, optimizer, epoch_size=100)
-#     trainer.step(401)
-#     assert len(trainer.history[0]["params"]) > len(trainer.history[-1]["params"])  # some stuff must be dead
-#     assert trainer.history[0]["loss"] > 0.03
-#     assert trainer.history[-1]["loss"] < 0.01
-#     assert trainer.history[-1]["loss_reg"] > 0.0
+if __name__ == "__main__":
+    test_die()
