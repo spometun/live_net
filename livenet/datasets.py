f8aee64909a8204b95290b75c9ff7466f6b69825
Serhii Pometun <spometun@solinkcorp.com>
2025-04-21 06:56:57 -0400

    cifar10 10x data augmentation

--- livenet/datasets.py
+++ livenet/datasets.py
@@ -1,20 +1,59 @@
 import numpy as np
 import torch
 import torchvision
+from scipy import ndimage
 import os
 
 datasets_dir = f"{os.getenv('HOME')}/datasets/research"
+
+
+def permutate_sync(data1, data2, rng=None):
+    assert len(data1) == len(data2)
+    if rng is None:
+        rng = np.random.default_rng(0)
+    permutation = rng.permutation(len(data1))
+    data1_perm = data1[permutation]
+    data2_perm = data2[permutation]
+    return data1_perm, data2_perm
 
 
 def augment_vertical_flip(data, labels):
     flipped = torch.flip(data, dims=[-1])
     augmented_x = torch.concatenate((data, flipped), dim=0)
     augmented_y = torch.concatenate((labels, labels), dim=0)
-    rng = np.random.default_rng(0)
-    permutation = rng.permutation(len(augmented_x))
-    augmented_x = augmented_x[permutation]
-    augmented_y = augmented_y[permutation]
+    augmented_x, augmented_y = permutate_sync(augmented_x, augmented_y)
     return augmented_x, augmented_y
+
+
+def augment_ten_rotation_shifts(data, labels, rotation_degrees: float, crop_pixels: int):
+    data = np.array(data.cpu())
+    labels = np.array(labels.cpu())
+    augmented_x = get_ten_rotation_shifts(data, rotation_degrees, crop_pixels)
+    augmented_x = np.concatenate((data, augmented_x), axis=0)
+    augmented_y = np.concatenate(11 * [labels], axis=0)
+    augmented_x, augmented_y = permutate_sync(augmented_x, augmented_y)
+    augmented_x = torch.tensor(augmented_x, device="cpu")
+    augmented_y = torch.tensor(augmented_y, device="cpu")
+    return augmented_x, augmented_y
+
+
+def get_ten_rotation_shifts(data: np.ndarray, rotation_degrees: float, crop_pixels: int):
+    assert data.ndim == 4 # assumes input is nchw
+    augmented = []
+
+    for angle in [-rotation_degrees, rotation_degrees]:
+        r = ndimage.rotate(data, angle, axes=(3, 2))
+        r = r[:, :, 1:-1,1:-1]
+        c = crop_pixels
+        augmented.append(r[:, :, :-c, :-c])
+        augmented.append(r[:, :, c:, :-c])
+        augmented.append(r[:, :, c:, c:])
+        augmented.append(r[:, :, :-c, c:])
+        augmented.append(r[:, :, c // 2:-c // 2, c // 2:-c // 2])
+
+    result = np.concatenate(augmented, axis=0)
+    return result
+
 
 def get_xor():
     xor_x = torch.Tensor([[0., 0.],
@@ -99,10 +138,8 @@ def get_cifar10_test():
     return _get_cifar10(False)
 
 
-def get_cifar10_train(augment=False):
+def get_cifar10_train():
     data, labels = _get_cifar10(True)
-    if augment:
-        data, labels = augment_vertical_flip(data, labels)
     return data, labels
 
 
