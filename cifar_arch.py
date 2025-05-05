ee097b59d18972c556c5709e31601a2f96c7f3b2
Serhii Pometun <spometun@solinkcorp.com>
2025-05-05 10:15:23 -0400

    last wip

--- cifar_arch.py
+++ cifar_arch.py
@@ -106,7 +106,7 @@ class EffNet(nn.Module):
 def conv_block_resnet9(in_channels, out_channels, pool=False):
     layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
               nn.BatchNorm2d(out_channels),
-              nn.ReLU(inplace=True)]
+              nn.ReLU6(inplace=True)]
     if pool: layers.append(nn.MaxPool2d(2))
     return nn.Sequential(*layers)
 
@@ -146,6 +146,51 @@ class ResNet9(nn.Module):
         size={0: 32, 1: 32, 2: 16, 3: 16, 4: 16, 5: 8, 6: 4, 7:4}
         params = list(self.parameters())
         for i in range(8):
+            mult = size[i] * size[i]
+            ind = i * 4
+            p = params[ind]
+            summa = torch.sum(torch.abs(p))
+            loss += self._alpha * mult * summa
+        # for param in self.parameters():
+        #     if len(param.data.shape) > 1:
+        #         loss += self._alpha * torch.sum(torch.abs(param)) / param.data.numel()
+        #         loss += self._alpha * torch.sum(torch.square(param)) / param.data.numel()
+        return loss
+
+
+class ResNet9Small(nn.Module):
+    def __init__(self, in_channels, num_classes, device):
+        super().__init__()
+        self.device = device
+        self.context = Context(self)
+        self._alpha = 0.05
+
+        self.conv1 = conv_block_resnet9(in_channels, 16)
+        self.conv2 = conv_block_resnet9(16, 32, pool=True)
+        self.res1 = nn.Sequential(conv_block_resnet9(32, 32), conv_block_resnet9(32, 32))
+
+        self.conv3 = conv_block_resnet9(32, 64, pool=True)
+        self.conv4 = conv_block_resnet9(64, 128, pool=True)
+        self.res2 = nn.Sequential(conv_block_resnet9(128, 128), conv_block_resnet9(128, 128))
+
+        self.classifier = nn.Sequential(nn.MaxPool2d(4), nn.Conv2d(128, 10, 1), nn.Flatten())
+        self.to(self.device)
+
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out
+
+    def internal_loss(self):
+        loss = torch.tensor(0., device=self.device)
+        size={0: 32, 1: 32, 2: 16, 3: 16, 4: 16, 5: 8, 6: 4, 7:4, 8:1}
+        params = list(self.parameters())
+        for i in range(9):
             mult = size[i] * size[i]
             ind = i * 4
             p = params[ind]
