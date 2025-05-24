9c1d13ccf7f80409ad5bb774a0234014fc811c3d
Serhii Pometun <sergiy.pometun@gmail.com>
2025-05-24 18:56:55 -0400

    + slow naive loop forward for SmartConv

--- livenet/v2/smart_conv.py
+++ livenet/v2/smart_conv.py
@@ -29,6 +29,43 @@ class SmartConv(nn.Module):
         self._reset_parameters()
 
     def forward(self, x: Tensor) -> Tensor:
+        # output = self._forward_im2col(x)
+        output = self._forward_direct_loop(x)
+        return output
+
+    def _forward_direct_loop(self, x: Tensor) -> Tensor:
+        N, C_in, H, W = x.shape
+        assert C_in == self.in_channels, f"Actual data input channels ({C_in}) does not match conv input channels ({self.in_channels})"
+        kh, kw = self.kernel_size
+        ph, pw = self.padding
+        groups = self.groups
+        out_h = H + 2 * ph - kh + 1
+        out_w = W + 2 * pw - kw + 1
+        wg = self.weight
+        len_g = wg.shape[1]
+        xg = x.view(N, groups, C_in // groups, H, W)
+        out = torch.empty(N, self.out_channels, out_h, out_w, device=x.device)
+        for g in range(groups):
+            for h0 in range(-ph, -ph + out_h):
+                for w0 in range(-pw, -pw + out_w):
+                    h1 = h0 + kh
+                    w1 = w0 + kw
+                    dh0 = max(0, -h0)
+                    dw0 = max(0, -w0)
+                    dh1 = max(h1 - H, 0)
+                    dw1 = max(w1 - W, 0)
+                    feature_x = xg[:, g, :, h0 + dh0:h1 - dh1, w0 + dw0:w1 - dw1]
+                    w = wg[g, :, :, dh0:kh-dh1, dw0:kw-dw1]
+                    feature_x_ = feature_x.reshape(N, -1)
+                    w_ = w.reshape(len_g, -1)
+                    out_hw = feature_x_ @ w_.T
+                    out[:, g*len_g:(g+1)*len_g, h0+ph, w0+pw] = out_hw
+        if self.bias is not None:
+            out += self.bias[:, None, None]
+        return out
+
+
+    def _forward_im2col(self, x: Tensor) -> Tensor:
         N, C_in, H, W = x.shape
         assert C_in == self.in_channels, f"Actual data input channels ({C_in}) does not match conv input channels ({self.in_channels})"
         kh, kw = self.kernel_size
@@ -62,11 +99,10 @@ class SmartConv(nn.Module):
                 
                 
 def pytest_configure(config):
-    # Ensure reproducibility
     torch.manual_seed(0)
 
 @pytest.fixture(params=[
-    {'in_channels': 8, 'out_channels': 6, 'kernel_size': 3, 'padding': 1, 'groups': 1, 'batch_size': 2, 'H': 5, 'W': 5},
+    {'in_channels': 8, 'out_channels': 6, 'kernel_size': 3, 'padding': 1, 'groups': 1, 'batch_size': 1, 'H': 5, 'W': 5},
     {'in_channels': 8, 'out_channels': 6, 'kernel_size': 3, 'padding': 1, 'groups': 2, 'batch_size': 2, 'H': 5, 'W': 4},
     {'in_channels': 8, 'out_channels': 6, 'kernel_size': (3, 1), 'padding': (1, 0), 'groups': 2, 'batch_size': 7, 'H': 8, 'W': 5},
     {'in_channels': 8, 'out_channels': 16, 'kernel_size': (3, 5), 'padding': (0, 0), 'groups': 8, 'batch_size': 7, 'H': 11, 'W': 5},
